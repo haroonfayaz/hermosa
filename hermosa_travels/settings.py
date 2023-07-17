1228a43642fdbf0fbe8d4c19d836435254fae019
Haroon fayaz <harounfayaz@gmail.com>
2023-07-17 16:04:20 +0530

    add githun

--- hermosa_travels/settings.py
+++ hermosa_travels/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os 
+
 
 
 
@@ -13,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lc&j8z6n-@=f(mzx!s)20r!%n@nz3t8tgl6t04h9t13hbz+69p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -112,6 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
